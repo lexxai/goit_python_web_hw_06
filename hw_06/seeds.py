c7c42a7532e4655bf0a7a6451622aa8c332841a8
lexxai <lexxai@users.noreply.github.com>
2023-09-24 04:22:37 +0300

    seeds

--- hw_06/seeds.py
+++ hw_06/seeds.py
@@ -1,8 +1,8 @@
 import logging
 from faker import Faker
-from datetime import datetime
-from random import randint, choice
-import sqlite3
+from datetime import datetime, date, timedelta
+from random import randint
+from sqlite3 import Cursor, ProgrammingError, Error
 
 try:
     from connection import create_connection
@@ -44,63 +44,114 @@ groups = [
 ]
 
 TOTAL_TEACHERS = 10
-TOTAL_STUDENDS = 150
+TOTAL_STUDENTS = 150
+TOTAL_GRADES = 400
 
 fake: Faker = Faker("uk-UA")
 
 
-def seed_disciplines(cur: sqlite3.Cursor):
-    sql = 'INSERT INTO disciplines(name, teachers_id) VALUES (?,?);'
+def seed_disciplines(cur: Cursor):
+    sql = "INSERT INTO disciplines(name, teachers_id) VALUES (?,?);"
     try:
-        cur.executemany(sql, zip(disciplines, [ randint(1,TOTAL_TEACHERS)  for _ in range(TOTAL_TEACHERS)] ))
-    except sqlite3.ProgrammingError as e:
+        cur.executemany(
+            sql,
+            zip(
+                disciplines,
+                iter([randint(1, TOTAL_TEACHERS) for _ in range(len(disciplines))]),
+            ),
+        )
+    except ProgrammingError as e:
         logger.error(e)
 
 
-def seed_groups(cur: sqlite3.Cursor):
-    sql = 'INSERT INTO groups(name) VALUES (?);'
+def seed_groups(cur: Cursor):
+    sql = "INSERT INTO groups(name) VALUES (?);"
     try:
-        cur.executemany(sql, zip(groups,))
-    except sqlite3.ProgrammingError as e:
+        cur.executemany(
+            sql,
+            zip(
+                groups,
+            ),
+        )
+    except ProgrammingError as e:
         logger.error(e)
 
 
-def seed_teacher(cur: sqlite3.Cursor):
-    teachers = [fake.name().replace("пані","").replace("пан","") for _ in range(TOTAL_TEACHERS)]
-    sql = 'INSERT INTO teachers(fullname) VALUES (?);'
+def seed_teacher(cur: Cursor):
+    teachers = [
+        fake.name().replace("пані", "").replace("пан", "")
+        for _ in range(TOTAL_TEACHERS)
+    ]
+    sql = "INSERT INTO teachers(fullname) VALUES (?);"
     try:
-        cur.executemany(sql, zip(teachers,))
-    except sqlite3.ProgrammingError as e:
+        cur.executemany(
+            sql,
+            zip(
+                teachers,
+            ),
+        )
+    except ProgrammingError as e:
         logger.error(e)
 
-def seed_students(cur: sqlite3.Cursor):
-    students = [fake.name().replace("пані","").replace("пан","") for _ in range(TOTAL_STUDENDS)]
-    sql = 'INSERT INTO disciplines(name, teachers_id) VALUES (?,?);'
+
+def seed_students(cur: Cursor):
+    students = [
+        fake.name().replace("пані", "").replace("пан", "")
+        for _ in range(TOTAL_STUDENTS)
+    ]
+    sql = "INSERT INTO students(fullname, group_id) VALUES (?,?);"
 
     try:
-        cur.executemany(sql, zip(students, [ randint(1,len(groups))  for _ in groups] ))
-    except sqlite3.ProgrammingError as e:
+        cur.executemany(
+            sql,
+            zip(
+                students, iter([randint(1, len(groups)) for _ in range(TOTAL_STUDENTS)])
+            ),
+        )
+    except ProgrammingError as e:
         logger.error(e)
 
-def seed_disciplines(cur: sqlite3.Cursor):
-    sql = 'INSERT INTO disciplines(name, teachers_id) VALUES (?,?);'
+
+def seed_grade(cur: Cursor):
+    satrt_date = datetime.strptime("2023-04-21", "%Y-%m-%d")
+    end_date = datetime.strptime("2024-02-20", "%Y-%m-%d")
+
+    def get_day() -> date:
+        fake_day: date
+        while True:
+            fake_day = fake.date_between(satrt_date, end_date)
+            if fake_day.isoweekday() < 6:
+                break
+        return fake_day
+
+    grades = []
+    sql = "INSERT INTO grade(grade, disciplines_id, students_id, date_of) VALUES (?, ?, ?, ?);"
+    for _ in range(TOTAL_GRADES):
+        random_discipline = randint(1, len(disciplines))
+        random_students = [randint(1, TOTAL_STUDENTS) for _ in range(randint(3, 12))]
+        random_date_of = get_day()
+        for random_student in random_students:
+            random_grade = randint(30, 100)
+            grades.append(
+                (random_grade, random_discipline, random_student, random_date_of)
+            )
     try:
-        cur.executemany(sql, zip(disciplines, [ randint(1,TOTAL_TEACHERS)  for _ in range(TOTAL_TEACHERS)] ))
-    except sqlite3.ProgrammingError as e:
+        cur.executemany(sql, grades)
+    except Error as e:
         logger.error(e)
-
 
 
 def seeds():
-    logger.debug("insert_data")
+    logger.debug("seeds data")
     try:
         with create_connection() as conn:
             if conn is not None:
-                cur: sqlite3.Cursor =  conn.cursor()
+                cur: Cursor = conn.cursor()
                 seed_disciplines(cur)
                 seed_groups(cur)
                 seed_teacher(cur)
                 seed_students(cur)
+                seed_grade(cur)
                 cur.close()
 
     except RuntimeError as err:
