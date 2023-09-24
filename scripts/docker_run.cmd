595a757b436034a027215920d320ce6709ac0712
lexxai <lexxai@users.noreply.github.com>
2023-09-25 00:47:53 +0300

    docker tune run

--- scripts/docker_run.cmd
+++ scripts/docker_run.cmd
@@ -1,9 +1,9 @@
 @echo off
 PUSHD ..\tests
 
-docker run -it -d --rm  -v web_hw_06_volume:/app/db  --name web_hw_06  lexxai/web_hw_06
+docker run -it --rm  -v web_hw_06_volume:/app/db  --name web_hw_06  lexxai/web_hw_06
 
-docker volume ls
+rem docker volume ls
                     
 
 POPD
