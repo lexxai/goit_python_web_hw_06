a22d41f7f9dbef6982bdcc2a78eb608e5aa47c57
lexxai <lexxai@users.noreply.github.com>
2023-09-25 19:00:48 +0300

    tune 12

--- sql/query_12.sql
+++ sql/query_12.sql
@@ -5,11 +5,10 @@ LEFT JOIN disciplines d ON g.disciplines_id = d.id
 LEFT JOIN teachers t ON d.teachers_id = t.id 
 LEFT JOIN groups gr ON s.group_id = gr.id 
 WHERE gr.id = 3 AND d.id = 1  
-AND date_of = (SELECT MAX(date_of)
-				FROM grade g
-				LEFT JOIN students s ON s.id = g.students_id 
-				LEFT JOIN disciplines d ON g.disciplines_id = d.id 
-				LEFT JOIN groups gr ON s.group_id = gr.id 
-				WHERE gr.id = 3 AND d.id = 1
-			   )
+AND date_of = (
+	SELECT MAX(date_of)
+	FROM grade g
+	LEFT JOIN students s ON s.id = g.students_id 
+	WHERE s.group_id = 3 AND g.disciplines_id = 1
+)
 ORDER BY grade DESC;
